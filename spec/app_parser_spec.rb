f28f191ecca5e94780f79f739e3eb5714482faa9
Hugo Sequeira <hugoandresequeira@gmail.com>
2016-03-28 17:28:28 +0100

    Add tests for the main methods

---
+++ spec/app_parser_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+
+describe AppParser do
+  let(:name) { '<div itemprop="name"><div>Bob</div></div>' }
+  let(:app_name) { Nokogiri::HTML(name) }
+  let(:app_detail) { Nokogiri::HTML('<div itemprop="name"><div>Bob - The best app</div></div>') }
+  let(:app_description) { Nokogiri::HTML(name + '<div itemprop="description">Yolo</div>') }
+  let(:app_icon) { Nokogiri::HTML(name + '<img itemprop="image" src="//icon.png">') }
+
+  describe '#new' do
+    it 'creates an app parser' do
+      parser = described_class.new('com.my.app', '<html></html>')
+
+      expect(parser.instance_variable_get(:@id)).to eq('com.my.app')
+      expect(parser.instance_variable_get(:@body)).to eq('<html></html>')
+    end
+  end
+
+  describe '#to_hash' do
+    it 'scrapes the app name' do
+      parser = described_class.new('id', app_name)
+
+      expect(parser.to_hash[:name]).to eq('Bob')
+    end
+
+    it 'scrapes the first part of the name if there is some description in the title' do
+      parser = described_class.new('id', app_name)
+
+      expect(parser.to_hash[:name]).to eq('Bob')
+    end
+
+    it 'scrapes the app description' do
+      parser = described_class.new('id', app_detail)
+
+      expect(parser.to_hash[:name]).to eq('Bob')
+    end
+
+    it "scrapes the app's icon url" do
+      parser = described_class.new('id', app_icon)
+
+      expect(parser.to_hash[:icon_url]).to eq('http://icon.png')
+    end
+  end
+end
--- spec/play_store_info_spec.rb
+++ spec/play_store_info_spec.rb
@@ -1,27 +1,54 @@
 require 'spec_helper'
 
 describe PlayStoreInfo do
+  let(:invalid_url) { 'http://google.com' }
+  let(:dummy_url) { 'https://play.google.com/store/apps/details?id=com.your.app&hl=en' }
+  let(:airbnb) { 'https://play.google.com/store/apps/details?id=com.airbnb.android&hl=en' }
+
   it 'has a version number' do
     expect(PlayStoreInfo::VERSION).not_to be nil
   end
 
   describe '.read' do
-    it 'raises error if the URL does not follow the right format' do
-      VCR.use_cassette('wrong_url') do
-        expect do
-          described_class.read('http://google.com')
-        end.to raise_error(PlayStoreInfo::InvalidStoreLink)
+    context 'something wrong' do
+      it 'raises error if the URL does not follow the right format' do
+        VCR.use_cassette('invalid_url') do
+          expect do
+            described_class.read(invalid_url)
+          end.to raise_error(PlayStoreInfo::InvalidStoreLink)
+        end
+      end
+
+      it 'raises error if the URL points to an app that does not exists' do
+        VCR.use_cassette('dummy_url') do
+          expect do
+            described_class.read(dummy_url)
+          end.to raise_error(PlayStoreInfo::AppNotFound)
+        end
+      end
+
+      it 'raises error if the Play Store is currently offline' do
+        VCR.use_cassette('offline_store') do
+          # no way to distingish between having the store offline or an app that doesn't exist
+          stub_request(:get, /play\.google\.com/).to_return(status: 404, body: '{}')
+
+          expect do
+            described_class.read(airbnb)
+          end.to raise_error(PlayStoreInfo::AppNotFound)
+        end
       end
     end
-  end
 
-  describe '.parse' do
-    it 'raises error if the URL points to an app that does not exists' do
-      VCR.use_cassette('invalid_url') do
-        invalid_url = 'https://play.google.com/store/apps/details?id=com.your.app&hl=en'
-        expect do
-          described_class.read(invalid_url)
-        end.to raise_error(PlayStoreInfo::AppNotFound)
+    context 'with a valid URL' do
+      it 'parses the Play Store and retrieves every data' do
+        VCR.use_cassette('parse_airbnb') do
+          parser = described_class.read(airbnb)
+
+          expect(parser[:id]).to eq('com.airbnb.android')
+          expect(parser[:name]).to eq('Airbnb')
+          expect(parser[:icon_url]).to start_with('http://lh6.ggpht.com/4jnm0-_9TBUdvNtQpefYE0T33')
+          expect(parser[:description]).to start_with('Make travel planning as mobile as you are')
+        end
       end
     end
   end
